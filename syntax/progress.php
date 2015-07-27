8950ac2958e9649e74519b7e9c264761b50064ed
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2015-07-27 22:45:52 +0200

    Updated author and comments in Progress and Bars

--- syntax/progress.php
+++ syntax/progress.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * Bootstrap Wrapper Plugin: List Group
+ * Bootstrap Wrapper Plugin: Progress
  * 
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     HavocKKS
  * @author     Giuseppe Di Terlizzi <giuseppe.diterlizzi>
  * @copyright  (C) 2015, Giuseppe Di Terlizzi
  */
@@ -14,8 +15,8 @@ require_once(dirname(__FILE__).'/bootstrap.php');
 
 class syntax_plugin_bootswrapper_progress extends syntax_plugin_bootswrapper_bootstrap {
 
-    protected $pattern_start = '<progress>';
-    protected $pattern_end   = '</progress>';
+    protected $pattern_start  = '<progress>';
+    protected $pattern_end    = '</progress>';
 
     protected $template_start = '<div class="progress">';
     protected $template_end   = '</div>';
--- syntax/progressbar.php
+++ syntax/progressbar.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * Bootstrap Wrapper Plugin: List Group
+ * Bootstrap Wrapper Plugin: Progress Bar
  * 
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     HavocKKS
  * @author     Giuseppe Di Terlizzi <giuseppe.diterlizzi>
  * @copyright  (C) 2015, Giuseppe Di Terlizzi
  */
@@ -14,33 +15,34 @@ require_once(dirname(__FILE__).'/bootstrap.php');
 
 class syntax_plugin_bootswrapper_progressbar extends syntax_plugin_bootswrapper_bootstrap {
 
-	protected $pattern_start  = '<(?:bar).*?>(?=.*?</(?:bar)>)';
+    protected $pattern_start  = '<(?:bar).*?>(?=.*?</(?:bar)>)';
     protected $pattern_end    = '</(?:bar)>';
     protected $tag_attributes = array(
-		'type'      => array('type'     => 'string',
-			'values'   => array('success', 'info', 'warning', 'danger'),
-			'required' => false,
-			'default'  => 'info'),
 
-		'value'   => array('type'     => 'string',
-			'values'   => null,
-			'required' => true,
-			'default'  => "0"),
-			
-		'striped'   => array('type'     => 'string',
-			'values'   => array(0, 1),
-			'required' => false,
-			'default'  => false),
-			
-		'showvalue'   => array('type'     => 'string',
-			'values'   => array(0, 1),
-			'required' => false,
-			'default'  => false),
-			
-		'animate'   => array('type'     => 'string',
-			'values'   => array(0, 1),
-			'required' => false,
-			'default'  => false),
+        'type'     => array('type'     => 'string',
+                            'values'   => array('success', 'info', 'warning', 'danger'),
+                            'required' => false,
+                            'default'  => 'info'),
+
+        'value'    => array('type'     => 'string',
+                            'values'   => null,
+                            'required' => true,
+                            'default'  => "0"),
+
+        'striped'  => array('type'     => 'string',
+                            'values'   => array(0, 1),
+                            'required' => false,
+                            'default'  => false),
+
+        'showvalue' => array('type'     => 'string',
+                            'values'   => array(0, 1),
+                            'required' => false,
+                            'default'  => false),
+
+        'animate'  => array('type'     => 'string',
+                            'values'   => array(0, 1),
+                            'required' => false,
+                            'default'  => false),
     );
 
     function getPType(){ return 'block'; }
@@ -59,26 +61,26 @@ class syntax_plugin_bootswrapper_progressbar extends syntax_plugin_bootswrapper_
                 case DOKU_LEXER_ENTER:
 
                     extract($attributes);
-					
-					$classCode = "";
-					if($striped){
-						$classCode = "progress-bar-striped";
-					}
-					
-					if($animate){
-						$classCode .= " active";
-					}
-					
+
+                    $classCode = "";
+                    if($striped){
+                        $classCode = "progress-bar-striped";
+                    }
+
+                    if($animate){
+                        $classCode .= " active";
+                    }
+
                     $markup = sprintf('<div class="progress-bar progress-bar-%s %s" role="progressbar" style="width: %s%%;">',
                                       $type, $classCode, $value, $label);
 
-					if($showvalue){
-						$markup .= sprintf('%s%% ', $value);
-					}
-					
-					if ($label) {
-						$markup .= sprintf('%s', $label);
-					}
+                    if($showvalue){
+                        $markup .= sprintf('%s%% ', $value);
+                    }
+
+                    if ($label) {
+                        $markup .= sprintf('%s', $label);
+                    }
 
                     $renderer->doc .= $markup;
                     return true;
@@ -88,8 +90,13 @@ class syntax_plugin_bootswrapper_progressbar extends syntax_plugin_bootswrapper_
                     return true;
 
             }
+
             return true;
+
         }
+
         return false;
+
     }
+
 }
