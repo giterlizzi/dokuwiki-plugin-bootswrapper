05832d2d67b16550a372cf20839b2a90e8edcbba
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2015-07-28 22:36:12 +0200

    Fixed author email

--- syntax/well.php
+++ syntax/well.php
@@ -3,7 +3,7 @@
  * Bootstrap Wrapper Plugin: Well
  * 
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Giuseppe Di Terlizzi <giuseppe.diterlizzi>
+ * @author     Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
  * @copyright  (C) 2015, Giuseppe Di Terlizzi
  */
  
