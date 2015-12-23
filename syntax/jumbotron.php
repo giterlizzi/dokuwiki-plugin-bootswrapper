bed12724648acf3418ef59fb5a3657158630af22
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2015-12-23 14:04:08 +0100

    Added new attributes for jumbotron (background and color)

--- syntax/jumbotron.php
+++ syntax/jumbotron.php
@@ -14,10 +14,69 @@ require_once(dirname(__FILE__).'/bootstrap.php');
 
 class syntax_plugin_bootswrapper_jumbotron extends syntax_plugin_bootswrapper_bootstrap {
 
-    protected $pattern_start  = '<(?:JUMBOTRON|jumbotron)>';
+    protected $pattern_start  = '<(?:JUMBOTRON|jumbotron).*?>(?=.*?</(?:JUMBOTRON|jumbotron)>)';
     protected $pattern_end    = '</(?:JUMBOTRON|jumbotron)>';
 
     protected $template_start = '<div class="bs-wrap bs-wrap-jumbotron jumbotron">';
     protected $template_end   = '</div>';
+
+    protected $tag_attributes = array(
+
+      'background' => array('type'     => 'string',
+                            'values'   => null,
+                            'required' => false,
+                            'default'  => null),
+
+      'color'      => array('type'     => 'string',
+                            'values'   => null,
+                            'required' => false,
+                            'default'  => null),
+
+    );
+
+    function render($mode, Doku_Renderer $renderer, $data) {
+
+        if (empty($data)) return false;
+
+        if ($mode == 'xhtml') {
+
+            /** @var Doku_Renderer_xhtml $renderer */
+            list($state, $match, $attributes, $is_block) = $data;
+
+            switch($state) {
+
+                case DOKU_LEXER_ENTER:
+
+                    $background = $attributes['background'];
+                    $color      = $attributes['color'];
+
+                    $styles = array();
+
+                    if ($background) {
+                      $styles[] = sprintf('background-image:url(%s)', ml($background));
+                    }
+
+                    if ($color) {
+                      $styles[] = sprintf('color:%s', hsc($color));
+                    }
+
+                    $markup = sprintf('<div class="bs-wrap bs-wrap-jumbotron jumbotron" style="%s">', implode(';', $styles), $type);
+
+                    $renderer->doc .= $markup;
+                    return true;
+
+                case DOKU_LEXER_EXIT:
+                    $renderer->doc .= '</div>';
+                    return true;
+
+            }
+
+            return true;
+
+        }
+
+        return false;
+
+    }
 
 }
