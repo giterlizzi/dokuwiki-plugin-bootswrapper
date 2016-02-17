ab470cd4cf891d398221144f374a1d1e8d701148
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2016-02-17 17:27:06 +0100

    Changed the default text color in Text syntax and other optimizations (thanks to @blacklord049 on #26 issue)

--- syntax/text.php
+++ syntax/text.php
@@ -4,7 +4,7 @@
  * 
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
- * @copyright  (C) 2015, Giuseppe Di Terlizzi
+ * @copyright  (C) 2015-2016, Giuseppe Di Terlizzi
  */
  
 // must be run within Dokuwiki
@@ -14,95 +14,99 @@ require_once(dirname(__FILE__).'/bootstrap.php');
 
 class syntax_plugin_bootswrapper_text extends syntax_plugin_bootswrapper_bootstrap {
 
-    protected $pattern_start = '<(?:TEXT|text).*?>(?=.*?</(?:TEXT|text)>)';
-    protected $pattern_end   = '</(?:TEXT|text)>';
-    protected $tag_attributes = array(
+  protected $pattern_start = '<(?:TEXT|text).*?>(?=.*?</(?:TEXT|text)>)';
+  protected $pattern_end   = '</(?:TEXT|text)>';
+  protected $tag_attributes = array(
 
-      'type'       => array('type'     => 'string',
-                            'values'   => array('muted', 'primary', 'success', 'info', 'warning', 'danger'),
-                            'required' => false,
-                            'default'  => 'muted'),
+    'type'       => array('type'     => 'string',
+                          'values'   => array('muted', 'primary', 'success', 'info', 'warning', 'danger'),
+                          'required' => false,
+                          'default'  => null),
 
-      'size'       => array('type'     => 'string',
-                            'values'   => null,
-                            'required' => false,
-                            'default'  => null),
+    'size'       => array('type'     => 'string',
+                          'values'   => null,
+                          'required' => false,
+                          'default'  => null),
 
-      'background' => array('type'     => 'string',
-                            'values'   => array('primary', 'success', 'info', 'warning', 'danger'),
-                            'required' => false,
-                            'default'  => null),
+    'background' => array('type'     => 'string',
+                          'values'   => array('primary', 'success', 'info', 'warning', 'danger'),
+                          'required' => false,
+                          'default'  => null),
 
-      'align'      => array('type'     => 'string',
-                            'values'   => array('left', 'center', 'right', 'justify', 'nowrap'),
-                            'required' => false,
-                            'default'  => null),
+    'align'      => array('type'     => 'string',
+                          'values'   => array('left', 'center', 'right', 'justify', 'nowrap'),
+                          'required' => false,
+                          'default'  => null),
 
-      'transform'  => array('type'     => 'string',
-                            'values'   => array('lowercase', 'uppercase', 'capitalize'),
-                            'required' => false,
-                            'default'  => null),
+    'transform'  => array('type'     => 'string',
+                          'values'   => array('lowercase', 'uppercase', 'capitalize'),
+                          'required' => false,
+                          'default'  => null),
 
-    );
+  );
 
-    function getPType() { return 'normal';}
+  function getPType() { return 'normal'; }
 
-    function render($mode, Doku_Renderer $renderer, $data) {
+  function render($mode, Doku_Renderer $renderer, $data) {
 
-        if (empty($data)) return false;
+    if (empty($data)) return false;
 
-        if ($mode == 'xhtml') {
+    if ($mode == 'xhtml') {
 
-            /** @var Doku_Renderer_xhtml $renderer */
-            list($state, $match, $attributes, $is_block) = $data;
+      /** @var Doku_Renderer_xhtml $renderer */
+      list($state, $match, $attributes, $is_block) = $data;
 
-            global $text_tag;
+      global $text_tag;
 
-            switch($state) {
+      switch($state) {
 
-                case DOKU_LEXER_ENTER:
+        case DOKU_LEXER_ENTER:
 
-                    $text_tag   = (($is_block) ? 'div' : 'span');
-                    $color      = $attributes['type'];
-                    $size       = $attributes['size'];
-                    $background = $attributes['background'];
-                    $align      = $attributes['align'];
-                    $transform  = $attributes['transform'];
+          $text_tag   = (($is_block) ? 'div' : 'span');
+          $color      = $attributes['type'];
+          $size       = $attributes['size'];
+          $background = $attributes['background'];
+          $align      = $attributes['align'];
+          $transform  = $attributes['transform'];
 
-                    $classes = array();
-                    $styles  = array();
+          $classes = array();
+          $styles  = array();
 
-                    if ($align)      { $classes[] = "text-$align"; }
-                    if ($color)      { $classes[] = "text-$color"; }
-                    if ($transform)  { $classes[] = "text-$transform"; }
-                    if ($background) { $classes[] = "bg-$background"; }
+          if ($align && $is_block) { $classes[] = "text-$align"; }
+          if ($color)              { $classes[] = "text-$color"; }
+          if ($transform)          { $classes[] = "text-$transform"; }
+          if ($background)         { $classes[] = "bg-$background"; }
 
-                    if (strtolower($size) == 'small') {
-                        $classes[] = 'small';
-                    } else {
-                        $styles[] = sprintf('font-size:%s', $size);
-                    }
+          if ($size) {
 
-                    $markup = sprintf('<%s class="bs-wrap bs-wrap-text text %s" style="%s">',
-                                      $text_tag,
-                                      implode(' ', $classes),
-                                      implode(';', $styles));
-
-                    $renderer->doc .= $markup;
-                    return true;
-
-                case DOKU_LEXER_EXIT:
-                    $renderer->doc .= "</$text_tag>";
-                    return true;
-
+            if (strtolower($size) == 'small') {
+              $classes[] = 'small';
+            } else {
+              $styles[] = sprintf('font-size:%s', $size);
             }
 
-            return true;
+          }
 
-        }
+          $markup = sprintf('<%s class="bs-wrap bs-wrap-text text %s" style="%s">',
+                            $text_tag,
+                            implode(' ', $classes),
+                            implode(';', $styles));
 
-        return false;
+          $renderer->doc .= $markup;
+          return true;
+
+        case DOKU_LEXER_EXIT:
+          $renderer->doc .= "</$text_tag>";
+          return true;
+
+      }
+
+      return true;
 
     }
+
+    return false;
+
+  }
 
 }
