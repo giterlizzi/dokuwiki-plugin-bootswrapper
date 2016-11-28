b446f3c7e08dd5df50021a3db069291963a5db82
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2016-11-28 16:45:08 +0100

    Updated Bootstrap CDN version to v.3.3.7 and disable loading of Bootstrap when the DokuWiki template is Bootstrap3 :P

--- action.php
+++ action.php
@@ -17,44 +17,46 @@ if(!defined('DOKU_INC')) die();
  */
 class action_plugin_bootswrapper extends DokuWiki_Action_Plugin {
 
-    /**
-     * Register events
-     *
-     * @param  Doku_Event_Handler  $controller
-     */
-    public function register(Doku_Event_Handler $controller) {
-        $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, '_load');
+  /**
+    * Register events
+    *
+    * @param  Doku_Event_Handler  $controller
+    */
+  public function register(Doku_Event_Handler $controller) {
+    $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, '_load');
+  }
+
+
+  /**
+    * Event handler
+    *
+    * @param  Doku_Event  &$event
+    */
+  public function _load(Doku_Event &$event, $param) {
+
+    global $conf;
+
+    if ($this->getConf('loadBootstrap') && $conf['template'] !== 'bootstrap3') {
+
+      $event->data['link'][] = array(
+        'type' => 'text/css',
+        'rel'  => 'stylesheet',
+        'href' => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+      );
+
+      $event->data['link'][] = array(
+        'type' => 'text/css',
+        'rel'  => 'stylesheet',
+        'href' => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css',
+      );
+
+      $event->data['script'][] = array(
+        'type' => 'text/javascript',
+        'src'  => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'
+      );
+
     }
 
-
-    /**
-     * Event handler
-     *
-     * @param  Doku_Event  &$event
-     */
-    public function _load(Doku_Event &$event, $param) {
-
-        if ($this->getConf('loadBootstrap')) {
-
-            $event->data['link'][] = array(
-              'type' => 'text/css',
-              'rel'  => 'stylesheet',
-              'href' => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
-            );
-
-            $event->data['link'][] = array(
-              'type' => 'text/css',
-              'rel'  => 'stylesheet',
-              'href' => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css',
-            );
-
-            $event->data['script'][] = array(
-              'type' => 'text/javascript',
-              'src'  => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'
-            );
-
-        }
-
-    }
+  }
 
 }
